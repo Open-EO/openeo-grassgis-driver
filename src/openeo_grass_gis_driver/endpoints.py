f44d0805a1a501cfb4ba2ac2a7a80d0baf4e80ea
huhabla <mo6i98ad>
2018-11-17 20:27:25 +0100

    More openEO API v0.3.1 support for processes

--- src/openeo_grass_gis_driver/endpoints.py
+++ src/openeo_grass_gis_driver/endpoints.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from .app import flask_api
 from .capabilities import Capabilities, OutputFormats, ServiceTypes
-from .data import Data
-from .data_product_id import DataProductId
-from .processes_process_id import ProcessesProcessId
+from .collections import Collections
+from .collection_information import CollectionInformationResource
 from .processes import Processes
 from .jobs import Jobs
 from .jobs_job_id import JobsJobId
-from .udf import Udf
-from .udf_lang_udf_type import UdfType
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -26,14 +23,10 @@ def create_endpoints():
     flask_api.add_resource(OutputFormats, '/output_formats')
     flask_api.add_resource(ServiceTypes, '/service_types')
 
-    flask_api.add_resource(Data, '/data')
-    flask_api.add_resource(DataProductId, '/data/<string:data_id>')
+    flask_api.add_resource(Collections, '/collections')
+    flask_api.add_resource(CollectionInformationResource, '/collections/<string:name>')
 
     flask_api.add_resource(Processes, '/processes')
-    flask_api.add_resource(ProcessesProcessId, '/processes/<string:process_id>')
 
     flask_api.add_resource(Jobs, '/jobs')
     flask_api.add_resource(JobsJobId, '/jobs/<string:job_id>')
-
-    flask_api.add_resource(Udf, '/udf')
-    flask_api.add_resource(UdfType, '/udf/<string:lang>/<string:udf_type>')

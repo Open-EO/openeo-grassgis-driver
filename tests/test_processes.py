b41f901c8efd1d611ba00f6b418ca203d4dd1fc8
huhabla <mo6i98ad>
2018-11-17 20:28:12 +0100

    More openEO API v0.3.1 support for processes

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+from pprint import pprint
 from flask import json
 from openeo_grass_gis_driver.test_base import TestBase
 
@@ -15,55 +16,9 @@ class ProcessesTestCase(TestBase):
     def test_processes(self):
         response = self.app.get('/processes')
         data = json.loads(response.data.decode())
-        print(data)
+        pprint(data)
 
-        self.assertEqual(len(data), 7)
-
-        dsets = ["filter_bbox",
-                 "filter_daterange",
-                 "min_time",
-                 "NDVI",
-                 "udf_reduce_time",
-                 "zonal_statistics",
-                 "raster_exporter"]
-
-        for entry in data:
-            self.assertTrue(entry in dsets)
-
-    def test_process_id_1(self):
-        response = self.app.get('/processes/filter_bbox')
-        data = json.loads(response.data.decode())
-        print(data)
-
-        self.assertEqual(data["process_id"], "filter_bbox")
-
-    def test_process_id_2(self):
-        response = self.app.get('/processes/filter_daterange')
-        data = json.loads(response.data.decode())
-        print(data)
-
-        self.assertEqual(data["process_id"], "filter_daterange")
-
-    def test_process_id_3(self):
-        response = self.app.get('/processes/NDVI')
-        data = json.loads(response.data.decode())
-        print(data)
-
-        self.assertEqual(data["process_id"], "NDVI")
-
-    def test_process_id_4(self):
-        response = self.app.get('/processes/min_time')
-        data = json.loads(response.data.decode())
-        print(data)
-
-        self.assertEqual(data["process_id"], "min_time")
-
-    def test_process_id_5(self):
-        response = self.app.get('/processes/udf_reduce_time')
-        data = json.loads(response.data.decode())
-        print(data)
-
-        self.assertEqual(data["process_id"], "udf_reduce_time")
+        self.assertEqual(len("processes"), 9)
 
 
 if __name__ == "__main__":

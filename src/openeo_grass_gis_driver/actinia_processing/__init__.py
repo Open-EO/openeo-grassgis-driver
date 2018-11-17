bec3f78f9181ba999e2e819143b713b2f8629cc1
huhabla <mo6i98ad>
2018-11-17 20:25:48 +0100

    More openEO API v0.3.1 support for processes

--- src/openeo_grass_gis_driver/actinia_processing/__init__.py
+++ src/openeo_grass_gis_driver/actinia_processing/__init__.py
@@ -2,9 +2,9 @@
 # Import the actinia_processing to fill the process.PROCESS_DICT with actinia_processing
 from . import filter_bbox_process
 from . import filter_daterange_process
+from . import get_data_process
 from . import ndvi_process
 from . import reduce_time_process
 # from . import udf_reduce_time
 from . import raster_exporter
-from . import get_data_process
 from . import zonal_statistics

268bc24de61cf7ac13d041b922ccc01d6716a7a5
huhabla <mo6i98ad>
2018-11-18 15:28:12 +0100

    Simple error response schema added

---
+++ src/openeo_grass_gis_driver/error_schemas.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""This file includes all required openEO response schemas
+"""
+from typing import List, Tuple, Optional, Dict
+from .schema_base import JsonableObject, EoLinks
+
+__author__ = "Sören Gebbert"
+__copyright__ = "Copyright 2018, Sören Gebbert, mundialis"
+__maintainer__ = "Sören Gebbert"
+__email__ = "soerengebbert@googlemail.com"
+
+
+class ErrorSchema(JsonableObject):
+    """This is the error response schema
+
+    """
+
+    def __init__(self, id: str, code: int, message: str,
+                 links: List[Optional[EoLinks]] = list()):
+
+        self.id = id
+        self.code = code
+        self.message = message
+        self.links = links
+

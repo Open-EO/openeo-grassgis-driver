48de9f6cc782a9566f5fe70a238ef39f0d707455
huhabla <mo6i98ad>
2019-01-26 14:56:36 +0100

    Removed deprecated code

--- src/openeo_grass_gis_driver/jobs.py
+++ src/openeo_grass_gis_driver/jobs.py
@@ -16,64 +16,12 @@ __email__ = "soerengebbert@googlemail.com"
 POST_JOBS_EXAMPLE = {"job_id": "42d5k3nd92mk49dmj294md"}
 
 
-POST_JOBS_DOC = {
-    "summary": "submits a new job to the back-end",
-    "description": "creates a new job from one or more (chained) processes at the back-end, "
-                   "which will eventually run the computations",
-    "tags": ["Job Management"],
-    "parameters": [
-        {
-            "name": "evaluate",
-            "in": "query",
-            "description": "Defines how the job should be evaluated. Can be `lazy` (the default), `batch`, or "
-                           "`sync` where lazy means that the job runs computations only on download requests "
-                           "considering dynamically provided views. Batch jobs are immediately scheduled for "
-                           "execution by the back-end. Synchronous jobs will be immediately executed and return "
-                           "the result data.",
-            "type": "string",
-            "enum": ["lazy", "batch", "sync"],
-            "default": "lazy",
-            "required": False
-        },
-        {
-            "name": "process_graph",
-            "in": "body",
-            "description": "Description of one or more (chained) processes including their input arguments",
-            "schema": ProcessGraph
-        },
-        {
-            "name": "format",
-            "in": "query",
-            "description": "Description of the desired output format. Required in case `evaluate` is set to `sync`. "
-                           "If not specified the format has to be specified in the download request.",
-            "type": "string",
-            "enum": ["nc", "json", "wcs", "wmts", "tms", "tif", "png", "jpeg"],
-            "required": False
-        }
-    ],
-    "responses": {
-        "200": {
-            "description": "Depending on the job evaluation type, the result of posting jobs can be either a json "
-                           "description of the job (for lazy and batch jobs) or a result object such as a NetCDF "
-                           "file (for sync jobs).",
-            "examples": {
-                "application/json": POST_JOBS_EXAMPLE
-            }
-        },
-        "406": {"description": "The server is not capable to deliver the requested format."},
-        "501": {"$ref": "#/responses/not_implemented"},
-        "503": {"$ref": "#/responses/unavailable"}
-    }
-}
-
-
 class Jobs(Resource):
 
     def __init__(self):
         self.iface = ActiniaInterface()
         self.db = GraphDB()
 
-    @swagger.doc(POST_JOBS_DOC)
     def put(self):
         """Modify the existing database by running the job in a persistent mapset
 
@@ -84,7 +32,9 @@ class Jobs(Resource):
 
             # Empty the process location
             ActiniaInterface.PROCESS_LOCATION = {}
-            process_graph = request.get_json()
+            request_doc = request.get_json()
+            process_graph = request_doc["process_graph"]
+
             # Transform the process graph into a process chain and store the input location
             # Check all locations in the process graph
             result_name, process_list = analyse_process_graph(process_graph)
@@ -129,7 +79,6 @@ class Jobs(Resource):
         except Exception as e:
                 return make_response(jsonify({"error": str(e)}), 400)
 
-    @swagger.doc(POST_JOBS_DOC)
     def post(self):
         """Run the job in an ephemeral mapset
 
@@ -139,7 +88,8 @@ class Jobs(Resource):
         try:
             # Empty the process location
             ActiniaInterface.PROCESS_LOCATION = {}
-            process_graph = request.get_json()
+            request_doc = request.get_json()
+            process_graph = request_doc["process_graph"]
             # Transform the process graph into a process chain and store the input location
             # Check all locations in the process graph
             result_name, process_list = analyse_process_graph(process_graph)

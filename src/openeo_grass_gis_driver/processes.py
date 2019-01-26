906eebc9abc7038cf91b2965257a92d0b7a6f709
huhabla <mo6i98ad>
2019-01-26 12:15:49 +0100

    We now have plenty of legacy code

--- src/openeo_grass_gis_driver/processes.py
+++ src/openeo_grass_gis_driver/processes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import make_response, jsonify
 from flask_restful import Resource
-from .actinia_processing.base import PROCESS_DESCRIPTION_DICT
+from openeo_grass_gis_driver.actinia_processing.base import PROCESS_DESCRIPTION_DICT_LEGACY
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -15,8 +15,8 @@ class Processes(Resource):
     def get(self):
 
         pd_list = list()
-        for key in PROCESS_DESCRIPTION_DICT:
-            pd = PROCESS_DESCRIPTION_DICT[key]
+        for key in PROCESS_DESCRIPTION_DICT_LEGACY:
+            pd = PROCESS_DESCRIPTION_DICT_LEGACY[key]
             pd_list.append(pd)
 
         response = dict(processes=pd_list, links=[])

27c82f0a21274227c88bd8a00a641092288bc8b8
huhabla <mo6i98ad>
2019-01-26 12:20:42 +0100

    Use upper case global dict names

--- tests/test_graph_validation_legacy.py
+++ tests/test_graph_validation_legacy.py
@@ -2,7 +2,7 @@
 import unittest
 from flask import json
 from openeo_grass_gis_driver.test_base import TestBase
-from openeo_grass_gis_driver.utils.process_graph_examples_legacy import ndvi_1, filter_bbox, openeo_usecase_1
+from openeo_grass_gis_driver.utils.process_graph_examples_legacy import NDVI_1, FILTER_BOX, OPENEO_USECASE_1
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -16,19 +16,19 @@ class GraphValidationTestCase(TestBase):
     def test_1_graph_filter_bbox_nc(self):
         """Run the validation test
         """
-        response = self.app.post('/validation', data=json.dumps(filter_bbox), content_type="application/json")
+        response = self.app.post('/validation', data=json.dumps(FILTER_BOX), content_type="application/json")
         self.assertEqual(response.status_code, 204)
 
     def test_2_graph_ndvi(self):
         """Run the validation test
         """
-        response = self.app.post('/validation', data=json.dumps(ndvi_1), content_type="application/json")
+        response = self.app.post('/validation', data=json.dumps(NDVI_1), content_type="application/json")
         self.assertEqual(response.status_code, 204)
 
     def test_3_use_case_1(self):
         """Run the validation test
         """
-        response = self.app.post('/validation', data=json.dumps(openeo_usecase_1), content_type="application/json")
+        response = self.app.post('/validation', data=json.dumps(OPENEO_USECASE_1), content_type="application/json")
         self.assertEqual(response.status_code, 204)
 
 

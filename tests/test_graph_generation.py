d9f78d4ee21a59762b4afd58fe1e6fbc726d7534
huhabla <mo6i98ad>
2019-01-26 12:15:14 +0100

    Consolidated the example graphs

--- tests/test_graph_generation.py
+++ tests/test_graph_generation.py
@@ -2,6 +2,8 @@
 import unittest
 from openeo_grass_gis_driver.test_base import TestBase
 from openeo_grass_gis_driver.actinia_processing.base import ProcessNode, ProcessGraph
+from openeo_grass_gis_driver.utils.process_graph_examples import OPENEO_EXAMPLE_1, \
+  FILTER_BBOX, GRAPH_NDVI_STRDS, USE_CASE_1
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -9,287 +11,12 @@ __copyright__ = "Copyright 2018, Sören Gebbert, mundialis"
 __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
-graph_filter_bbox_nc = {
-  "title": "Bounding box filtering of raster layer elevation",
-  "description": "This process graph applies the bounding box filter to a raster layer",
-  "process_graph": {
-    "filter_bbox_1": {
-      "process_id": "filter_bbox",
-      "arguments": {
-        "data": {"from_node": "get_data_1"},
-        "left": -40.5,
-        "right": 75.5,
-        "top": 75.5,
-        "bottom": 25.25,
-        "width_res": 0.1,
-        "height_res": 0.1,
-      }
-    },
-    "get_data_1": {
-      "process_id": "get_data",
-      "arguments": {
-        "data": {
-          "name": "nc_spm_08.PERMANENT.raster.elevation"
-        }
-      }
-    }
-  }
-}
-
-graph_ndvi_strds = {
-  "title": "Compute the NDVI based on two STRDS",
-  "description": "Compute the NDVI data from two space-time raster datasets",
-  "process_graph": {
-    "ndvi_1": {
-      "process_id": "NDVI",
-      "arguments": {
-        "red": {"from_node": "get_red_data"},
-        "nir": {"from_node": "get_nir_data"},
-      }
-    },
-    "get_red_data": {
-      "process_id": "get_data",
-      "arguments": {
-        "data": {
-          "name": "nc_spm_08.landsat.strds.lsat5_red"
-        }
-      }
-    },
-    "get_nir_data": {
-      "process_id": "get_data",
-      "arguments": {
-        "data": {
-          "name": "nc_spm_08.landsat.strds.lsat5_nir"
-        }
-      }
-    }
-  }
-}
-
-graph_use_case_1 = {
-  "title": "Compute the NDVI based on two STRDS",
-  "description": "Compute the NDVI data from two space-time raster datasets and apply several filters in the data",
-  "process_graph": {
-    "get_red_data": {
-      "process_id": "get_data",
-      "arguments": {
-        "data": {
-          "name": "nc_spm_08.landsat.strds.lsat5_red"
-        }
-      }
-    },
-    "get_nir_data": {
-      "process_id": "get_data",
-      "arguments": {
-        "data": {
-          "name": "nc_spm_08.landsat.strds.lsat5_nir"
-        }
-      }
-    },
-    "filter_bbox_red": {
-      "process_id": "filter_bbox",
-      "arguments": {
-        "data": {"from_node": "get_red_data"},
-        "left": -40.5,
-        "right": 75.5,
-        "top": 75.5,
-        "bottom": 25.25,
-        "width_res": 0.1,
-        "height_res": 0.1,
-      }
-    },
-    "filter_bbox_nir": {
-      "process_id": "filter_bbox",
-      "arguments": {
-        "data": {"from_node": "get_nir_data"},
-        "left": -40.5,
-        "right": 75.5,
-        "top": 75.5,
-        "bottom": 25.25,
-        "width_res": 0.1,
-        "height_res": 0.1,
-      }
-    },
-    "ndvi_1": {
-      "process_id": "NDVI",
-      "arguments": {
-        "red": {"from_node": "filter_bbox_red"},
-        "nir": {"from_node": "filter_bbox_nir"},
-      }
-    },
-    "filter_daterange_ndvi": {
-      "process_id": "filter_daterange",
-      "arguments": {
-        "data": {"from_node": "ndvi_1"},
-        "from": "2001-01-01",
-        "to": "2005-01-01",
-      }
-    },
-    "reduce_time_1": {
-      "process_id": "reduce_time",
-      "arguments": {
-        "data": {"from_node": "filter_daterange_ndvi"},
-        "method": "minimum",
-      }
-    }
-  }
-}
-
-example_graph = {
-  "title": "NDVI based on Sentinel 2",
-  "description": "Deriving minimum NDVI measurements over pixel time series of Sentinel 2",
-  "process_graph": {
-    "export1": {
-      "process_id": "export",
-      "arguments": {
-        "data": {
-          "from_node": "mergec1"
-        },
-        "format": "png"
-      }
-    },
-    "export2": {
-      "process_id": "export",
-      "arguments": {
-        "data": {
-          "from_node": "reduce2"
-        },
-        "format": "png"
-      },
-      "result": True
-    },
-    "filter1": {
-      "process_id": "filter_temporal",
-      "arguments": {
-        "data": {
-          "from_node": "getcol1"
-        },
-        "from": "2017-01-01",
-        "to": "2017-01-31"
-      }
-    },
-    "filter2": {
-      "process_id": "filter_temporal",
-      "arguments": {
-        "data": {
-          "from_node": "getcol1"
-        },
-        "from": "2018-01-01",
-        "to": "2018-01-31"
-      }
-    },
-    "filter3": {
-      "process_id": "filter_bands",
-      "arguments": {
-        "bands": [
-          "nir",
-          "red"
-        ],
-        "data": {
-          "from_node": "reduce1"
-        }
-      }
-    },
-    "getcol1": {
-      "process_id": "get_collection",
-      "arguments": {
-        "name": "Sentinel-1"
-      }
-    },
-    "mergec1": {
-      "process_id": "merge_collections",
-      "arguments": {
-        "data1": {
-          "from_node": "filter1"
-        },
-        "data2": {
-          "from_node": "filter2"
-        }
-      }
-    },
-    "reduce1": {
-      "process_id": "reduce",
-      "arguments": {
-        "data": {
-          "from_node": "mergec1"
-        },
-        "dimension": "temporal",
-        "reducer": {
-          "callback": {
-            "min1": {
-              "process_id": "min",
-              "arguments": {
-                "data": {
-                  "from_argument": "dimension_data"
-                },
-                "dimension": {
-                  "from_argument": "dimension"
-                }
-              },
-              "result": True
-            }
-          }
-        }
-      }
-    },
-    "reduce2": {
-      "process_id": "reduce",
-      "arguments": {
-        "data": {
-          "from_node": "filter3"
-        },
-        "dimension": "spectral",
-        "reducer": {
-          "callback": {
-            "divide1": {
-              "process_id": "divide",
-              "arguments": {
-                "x": {
-                  "from_node": "substr1"
-                },
-                "y": {
-                  "from_node": "sum1"
-                }
-              },
-              "result": True
-            },
-            "output1": {
-              "process_id": "output",
-              "arguments": {
-                "data": {
-                  "from_node": "divide1"
-                }
-              }
-            },
-            "substr1": {
-              "process_id": "substract",
-              "arguments": {
-                "data": {
-                  "from_argument": "dimension_data"
-                }
-              }
-            },
-            "sum1": {
-              "process_id": "sum",
-              "arguments": {
-                "data": {
-                  "from_argument": "dimension_data"
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
 
 class GraphValidationTestCase(TestBase):
 
     def test_graph_creation_example_graph(self):
 
-        pg = ProcessGraph(example_graph)
+        pg = ProcessGraph(OPENEO_EXAMPLE_1)
         self.assertEqual(2, len(pg.root_nodes))
         self.assertEqual(9, len(pg.node_dict))
 
@@ -305,7 +32,7 @@ class GraphValidationTestCase(TestBase):
 
     def test_graph_creation_graph_filter_bbox_nc(self):
 
-        pg = ProcessGraph(graph_filter_bbox_nc)
+        pg = ProcessGraph(FILTER_BBOX)
 
         print(pg.node_dict["get_data_1"])
         print(pg.node_dict["filter_bbox_1"])
@@ -320,7 +47,7 @@ class GraphValidationTestCase(TestBase):
 
     def test_graph_creation_graph_ndvi_strds(self):
 
-        pg = ProcessGraph(graph_ndvi_strds)
+        pg = ProcessGraph(GRAPH_NDVI_STRDS)
 
         print(pg.node_dict["get_nir_data"])
         print(pg.node_dict["get_red_data"])
@@ -341,7 +68,7 @@ class GraphValidationTestCase(TestBase):
 
         # TODO: implement assertion tests
 
-        pg = ProcessGraph(graph_use_case_1)
+        pg = ProcessGraph(USE_CASE_1)
 
         print(pg.node_dict["get_nir_data"])
         print(pg.node_dict["get_red_data"])

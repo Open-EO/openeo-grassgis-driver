aaccfe1103e9aa5fdff76c91a59f9cd9b7fc9e12
huhabla <mo6i98ad>
2019-01-26 14:55:38 +0100

    Rename process dicts

--- src/openeo_grass_gis_driver/processes.py
+++ src/openeo_grass_gis_driver/processes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import make_response, jsonify
 from flask_restful import Resource
-from openeo_grass_gis_driver.actinia_processing.base import PROCESS_DESCRIPTION_DICT_LEGACY
+from openeo_grass_gis_driver.actinia_processing.base import PROCESS_DESCRIPTION_DICT
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -15,8 +15,8 @@ class Processes(Resource):
     def get(self):
 
         pd_list = list()
-        for key in PROCESS_DESCRIPTION_DICT_LEGACY:
-            pd = PROCESS_DESCRIPTION_DICT_LEGACY[key]
+        for key in PROCESS_DESCRIPTION_DICT:
+            pd = PROCESS_DESCRIPTION_DICT[key]
             pd_list.append(pd)
 
         response = dict(processes=pd_list, links=[])

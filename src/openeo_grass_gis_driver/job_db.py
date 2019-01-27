dc65aa0edbc18a0895c53f30d1bfda678ade9fe9
huhabla <mo6i98ad>
2019-01-27 11:28:28 +0100

    Job database added

---
+++ src/openeo_grass_gis_driver/job_db.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from .actinia_processing.config import Config as ActiniaConfig
+from sqlitedict import SqliteDict
+
+__license__ = "Apache License, Version 2.0"
+__author__ = "Sören Gebbert"
+__copyright__ = "Copyright 2018, Sören Gebbert, mundialis"
+__maintainer__ = "Soeren Gebbert"
+__email__ = "soerengebbert@googlemail.com"
+
+
+class JobDB(SqliteDict):
+    """This is the storage of all jobs that where committed
+
+    """
+    def __init__(self):
+        SqliteDict.__init__(self, filename=ActiniaConfig.JOB_DB, autocommit=True)

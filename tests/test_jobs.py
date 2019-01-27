0ab1533e4b29453c6debcba211b06724c2012611
huhabla <mo6i98ad>
2019-01-27 16:40:57 +0100

    More job tests

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import unittest
-import time
 import pprint
 from flask import json
 from openeo_grass_gis_driver.test_base import TestBase
-from openeo_grass_gis_driver.actinia_processing import config
 from openeo_grass_gis_driver.utils.process_graph_examples_v03 import *
 
 __license__ = "Apache License, Version 2.0"
@@ -20,18 +18,81 @@ JOB_TEMPLATE = {
     "process_graph": None,
 }
 
+
 class JobsTestCase(TestBase):
 
-    def test_graph_filter_bbox_nc_job_ephemeral(self):
+    def setUp(self):
+        TestBase.setUp(self)
+        response = self.app.delete('/jobs')
+        self.assertEqual(204, response.status_code)
+
+    def test_job_creation_1(self):
         """Run the test in the ephemeral database
         """
-        JOB_TEMPLATE["process_graph"] = FILTER_BOX
-        response = self.app.post('/jobs', data=json.dumps(JOB_TEMPLATE), content_type="application/json")
+        JOB_TEMPLATE["process_graph"] = FILTER_BOX["process_graph"]
 
-        data = json.loads(response.data.decode())
+        response = self.app.post('/jobs', data=json.dumps(JOB_TEMPLATE), content_type="application/json")
+        self.assertEqual(201, response.status_code)
+        job_id = response.get_data().decode("utf-8")
+
+        response = self.app.get('/jobs')
+        self.assertEqual(200, response.status_code)
+
+        data = json.loads(response.get_data().decode("utf-8"))
         pprint.pprint(data)
 
-        self.wait_until_finished(response)
+        self.assertEqual(job_id, data["jobs"][0]["job_id"])
+
+        response = self.app.get(f'/jobs/{job_id}')
+        self.assertEqual(200, response.status_code)
+
+        data = json.loads(response.get_data().decode("utf-8"))
+        pprint.pprint(data)
+
+        self.assertEqual(job_id, data["job_id"])
+        self.assertEqual(FILTER_BOX["process_graph"], data["process_graph"])
+
+    def test_job_creation_2(self):
+        """Run the test in the ephemeral database
+        """
+        JOB_TEMPLATE["process_graph"] = ZONAL_STATISTICS["process_graph"]
+
+        response = self.app.post('/jobs', data=json.dumps(JOB_TEMPLATE), content_type="application/json")
+        self.assertEqual(201, response.status_code)
+        job_id = response.get_data().decode("utf-8")
+
+        response = self.app.get(f'/jobs/{job_id}')
+        self.assertEqual(200, response.status_code)
+
+        data = json.loads(response.get_data().decode("utf-8"))
+        pprint.pprint(data)
+        self.assertEqual(job_id, data["job_id"])
+        self.assertEqual(ZONAL_STATISTICS["process_graph"], data["process_graph"])
+
+        response = self.app.get(f'/jobs/{job_id}' + "_nope")
+        self.assertEqual(404, response.status_code)
+
+
+    def test_job_creation_deletion_1(self):
+        """Run the test in the ephemeral database
+        """
+        JOB_TEMPLATE["process_graph"] = ZONAL_STATISTICS["process_graph"]
+
+        response = self.app.post('/jobs', data=json.dumps(JOB_TEMPLATE), content_type="application/json")
+        self.assertEqual(201, response.status_code)
+        job_id = response.get_data().decode("utf-8")
+
+        response = self.app.get(f'/jobs/{job_id}')
+        self.assertEqual(200, response.status_code)
+
+        response = self.app.delete(f'/jobs/{job_id}')
+        self.assertEqual(204, response.status_code)
+
+        response = self.app.delete(f'/jobs/{job_id}')
+        self.assertEqual(404, response.status_code)
+
+        response = self.app.get(f'/jobs/{job_id}')
+        self.assertEqual(404, response.status_code)
 
 
 if __name__ == "__main__":

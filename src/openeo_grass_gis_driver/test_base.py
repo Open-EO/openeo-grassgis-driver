08d89571b5abb150fc3bc3046c37725eac5b8af9
huhabla <mo6i98ad>
2019-01-27 12:08:08 +0100

    Added wait_until_finished to test framework

--- src/openeo_grass_gis_driver/test_base.py
+++ src/openeo_grass_gis_driver/test_base.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
+from flask import json
+import time
 from .app import flask_api
 from .endpoints import create_endpoints
 from .actinia_processing.config import Config as ActiniaConfig
@@ -19,3 +21,44 @@ class TestBase(unittest.TestCase):
         self.app = flask_api.app.test_client()
         self.gconf = ActiniaConfig()
         self.gconf.PORT = "443"
+
+    def wait_until_finished(self, response, http_status=200, status="finished"):
+        """Poll the status of a resource and assert its finished HTTP status
+
+        The response will be checked if the resource was accepted. Hence it must always be HTTP 200 status.
+
+        The status URL from the response is then polled until status: finished, error or terminated.
+        The result of the poll can be checked against its HTTP status and its GRaaS status message.
+
+        Args:
+            response: The accept response
+            http_status (int): The HTTP status that should be checked
+            status (str): The return status of the response
+
+        Returns: response
+
+        """
+        # Check if the resource was accepted
+        self.assertEqual(response.status_code, 200, "HTML status code is wrong %i" % response.status_code)
+        self.assertEqual(response.mimetype, "application/json", "Wrong mimetype %s" % response.mimetype)
+
+        resp_data = json.loads(response.data.decode())
+
+        while True:
+            print("waiting for finished job")
+            response = self.app.get('/jobs/%s' % resp_data["job_id"])
+            resp_data = json.loads(response.data.decode())
+
+            if "status" not in resp_data:
+                raise Exception("wrong return values %s" % str(resp_data))
+            if resp_data["status"] == "finished" or \
+                    resp_data["status"] == "error" or \
+                    resp_data["status"] == "terminated":
+                break
+            time.sleep(0.2)
+
+        self.assertEqual(resp_data["status"], status)
+        self.assertEqual(response.status_code, http_status, "HTML status code is wrong %i" % response.status_code)
+
+        time.sleep(0.4)
+        return resp_data

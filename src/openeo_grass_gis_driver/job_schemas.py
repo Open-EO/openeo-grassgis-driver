0a55a9712a8dd9be8226ba8cd18e659afe46bbbe
huhabla <mo6i98ad>
2019-01-27 13:27:20 +0100

    Added missing definition and links to documentation

--- src/openeo_grass_gis_driver/job_schemas.py
+++ src/openeo_grass_gis_driver/job_schemas.py
@@ -11,17 +11,35 @@ __maintainer__ = "Sören Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
 
+class OutputFormat(JsonableObject):
+    """Information about the output format
+
+    https://open-eo.github.io/openeo-api/v/0.3.0/apireference/#tag/Job-Management/paths/~1jobs~1{job_id}/get
+    """
+
+    def __init__(self, format: str, parameters: Dict[str, str]):
+
+        self.format = format
+        self.parameters = parameters
+
+
 class JobInformation(JsonableObject):
     """Information about a job
+
+    https://open-eo.github.io/openeo-api/v/0.3.0/apireference/#tag/Job-Management/paths/~1jobs~1{job_id}/get
     """
 
     def __init__(self, job_id: str, title: str,
                  description: str, status: str,
+                 process_graph: dict(),
+                 output: OutputFormat,
                  submitted: datetime, updated: datetime,
                  plan: str = "free", cost: float= 0.0, budget: float = 0.0):
         self.job_id = job_id
         self.title = title
         self.description = description
+        self.process_graph = process_graph
+        self.output = output
         self.status = status
         self.submitted = submitted
         self.updated = updated
@@ -32,6 +50,8 @@ class JobInformation(JsonableObject):
 
 class JobList(JsonableObject):
     """A collection of job description entries
+
+    https://open-eo.github.io/openeo-api/v/0.3.0/apireference/#tag/Job-Management/paths/~1jobs/get
     """
 
     def __init__(self, jobs: List[JobInformation],

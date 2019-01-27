b306a487affdc381097717c4ee402cfebc001eb2
huhabla <mo6i98ad>
2019-01-27 12:09:43 +0100

    Added plenty of processes to the preview endpoint tests

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -6,6 +6,7 @@ from flask import json
 from openeo_grass_gis_driver.test_base import TestBase
 from openeo_grass_gis_driver.utils.process_graph_examples_v03 import *
 
+
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
 __copyright__ = "Copyright 2018, Sören Gebbert, mundialis"
@@ -17,28 +18,11 @@ PREVIEW_TEMPLATE = {
     "process_graph": None,
 }
 
-FILTER_BOX = {
-    "process_graph": {
-        "process_id": "filter_bbox",
-        "imagery": {
-            "process_id": "get_data",
-            "data_id": "nc_spm_08.PERMANENT.raster.elevation"
-        },
-        "spatial_extent": {
-            "left": 630000,
-            "right": 645000,
-            "top": 228500,
-            "bottom": 215000,
-            "width_res": 10,
-            "height_res": 10,
-        }
-    }
-}
 
 class PreviewTestCase(TestBase):
 
     def test_graph_filter_bbox_nc_job_ephemeral(self):
-        """Run the test in the ephemeral database
+        """Test the filter box process
         """
         PREVIEW_TEMPLATE["process_graph"] = FILTER_BOX
         response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json")
@@ -46,8 +30,10 @@ class PreviewTestCase(TestBase):
         data = json.loads(response.data.decode())
         pprint.pprint(data)
 
+        self.assertEqual(200, response.status_code)
+
     def test_graph_filter_bbox_nc_job_ephemeral_error(self):
-        """Run the test in the ephemeral database
+        """Test the filter box process that produces an error because of wrong raster name
         """
 
         fbox = FILTER_BOX
@@ -57,6 +43,63 @@ class PreviewTestCase(TestBase):
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
+
+        self.assertEqual(400, response.status_code)
+
+    def test_graph_get_data_1(self):
+        """Test the get data process to get raster data
+        """
+        PREVIEW_TEMPLATE["process_graph"] = GET_DATA_1
+        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json")
+
+        data = json.loads(response.data.decode())
+        pprint.pprint(data)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_graph_get_data_3(self):
+        """Test the get data process to get strds data
+        """
+        PREVIEW_TEMPLATE["process_graph"] = GET_DATA_3
+        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json")
+
+        data = json.loads(response.data.decode())
+        pprint.pprint(data)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_graph_daterange(self):
+        """Run the daterange process
+        """
+        PREVIEW_TEMPLATE["process_graph"] = DATERANGE
+        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json")
+
+        data = json.loads(response.data.decode())
+        pprint.pprint(data)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_graph_raster_export(self):
+        """Run the raster export process
+        """
+        PREVIEW_TEMPLATE["process_graph"] = RASTER_EXPORT
+        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json")
+
+        data = json.loads(response.data.decode())
+        pprint.pprint(data)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_graph_raster_zonal_statistics_single(self):
+        """Run the zonal statistics process
+        """
+        PREVIEW_TEMPLATE["process_graph"] = ZONAL_STATISTICS_SINGLE
+        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json")
+
+        data = json.loads(response.data.decode())
+        pprint.pprint(data)
+
+        self.assertEqual(200, response.status_code)
 
 
 if __name__ == "__main__":

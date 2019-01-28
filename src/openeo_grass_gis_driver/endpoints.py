574d229d20de066ad2fe816f656db50b21d31d00
huhabla <mo6i98ad>
2019-01-28 21:23:15 +0100

    Added process graph endpoints

--- src/openeo_grass_gis_driver/endpoints.py
+++ src/openeo_grass_gis_driver/endpoints.py
@@ -6,9 +6,11 @@ from openeo_grass_gis_driver.collection_information import CollectionInformation
 from openeo_grass_gis_driver.processes import Processes
 from openeo_grass_gis_driver.jobs import Jobs, OutputFormats
 from openeo_grass_gis_driver.jobs_job_id import JobsJobId
-from openeo_grass_gis_driver.graph_validation import GraphValidation
+from openeo_grass_gis_driver.process_graph_validation import GraphValidation
 from openeo_grass_gis_driver.preview import Preview
 from openeo_grass_gis_driver.jobs_job_id_results import JobsJobIdResults
+from openeo_grass_gis_driver.process_graphs import ProcessGraphs
+from openeo_grass_gis_driver.process_graphs_id import ProcessGraphId
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -19,8 +21,6 @@ __email__ = "soerengebbert@googlemail.com"
 
 def create_endpoints():
     """Create all endpoints for the openEO Core API  wrapper
-
-    :return:
     """
     flask_api.add_resource(Capabilities, '/')
     flask_api.add_resource(ServiceTypes, '/service_types')
@@ -29,9 +29,11 @@ def create_endpoints():
     flask_api.add_resource(CollectionInformationResource, '/collections/<string:name>')
 
     flask_api.add_resource(Processes, '/processes')
-    flask_api.add_resource(GraphValidation, '/validation')
 
+    flask_api.add_resource(GraphValidation, '/validation')
     flask_api.add_resource(Preview, '/preview')
+    flask_api.add_resource(ProcessGraphs, '/process_graphs')
+    flask_api.add_resource(ProcessGraphId, '/process_graphs/<string:process_graph_id>')
 
     flask_api.add_resource(OutputFormats, '/output_formats')
     flask_api.add_resource(Jobs, '/jobs')

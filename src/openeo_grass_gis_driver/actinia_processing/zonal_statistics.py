402af1cf3ab331e4513d11675f24031809a97b3e
huhabla <mo6i98ad>
2019-01-29 01:45:35 +0100

    Convert from api version 0.3.0 to api version 0.4.0

--- src/openeo_grass_gis_driver/actinia_processing/zonal_statistics.py
+++ src/openeo_grass_gis_driver/actinia_processing/zonal_statistics.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from random import randint
 import json
-from .base import analyse_process_graph, PROCESS_DICT, PROCESS_DESCRIPTION_DICT
+from .base import process_node_to_actinia_process_chain, PROCESS_DICT, PROCESS_DESCRIPTION_DICT
 from openeo_grass_gis_driver.process_schemas import Parameter, ProcessDescription, ReturnValue
 from .actinia_interface import ActiniaInterface
 
@@ -148,7 +148,7 @@ def get_process_list(process):
     """
 
     # Get the input description and the process chain to attach this process
-    input_names, process_list = analyse_process_graph(process)
+    input_names, process_list = process_node_to_actinia_process_chain(process)
     output_names = []
 
     for input_name in input_names:

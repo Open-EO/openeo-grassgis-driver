1ee0eed3b44985a315f139a5db0c7b345c2ad118
huhabla <mo6i98ad>
2019-01-29 01:45:12 +0100

    Convert from api version 0.3.0 to api version 0.4.0

--- src/openeo_grass_gis_driver/actinia_processing/ndvi_process.py
+++ src/openeo_grass_gis_driver/actinia_processing/ndvi_process.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from random import randint
 import json
-from .base import analyse_process_graph, PROCESS_DICT, PROCESS_DESCRIPTION_DICT
+from .base import process_node_to_actinia_process_chain, PROCESS_DICT, PROCESS_DESCRIPTION_DICT
 from openeo_grass_gis_driver.process_schemas import Parameter, ProcessDescription, ReturnValue
 from .actinia_interface import ActiniaInterface
 
@@ -119,7 +119,7 @@ def get_process_list(process):
 
     red_strds = None
     nir_strds = None
-    input_names, process_list = analyse_process_graph(process)
+    input_names, process_list = process_node_to_actinia_process_chain(process)
 
     # Find the red and nir datasets in the input
     for input_name in input_names:

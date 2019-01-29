78872f55f0374db58ef03849069bc5fbf5c6027a
huhabla <mo6i98ad>
2019-01-29 01:44:34 +0100

    Convert from api version 0.3.0 to api version 0.4.0

--- src/openeo_grass_gis_driver/actinia_processing/base.py
+++ src/openeo_grass_gis_driver/actinia_processing/base.py
@@ -14,36 +14,6 @@ __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
 
-def analyse_process_graph(graph: dict):
-    """Analyse a process graph and call the required subprocess analysis
-
-    This function return the list of input names for the next process and the
-    Actinia process chain that was build before.
-
-    :param graph: The process description
-    :return: (output_name_list, process_list)
-    """
-
-    if graph is None:
-        raise Exception("Empty process graph")
-
-    process_list = []
-    output_name_list = []
-
-    for key in graph:
-        process = graph[key]
-
-        if "process_id" in process:
-
-            if process["process_id"] not in PROCESS_DICT:
-                raise Exception("Unsupported process id, available processes: %s" % PROCESS_DICT.keys())
-
-            outputs, processes = PROCESS_DICT[process["process_id"]](process)
-            process_list.extend(processes)
-            output_name_list.extend(outputs)
-
-    return output_name_list, process_list
-
 ###############################################################################
 ####### Version 0.4 of the API ################################################
 ###############################################################################

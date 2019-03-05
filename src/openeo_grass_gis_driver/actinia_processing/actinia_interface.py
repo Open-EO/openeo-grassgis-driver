4a8ec4e9e3d6f93684d46999ffccd51e90dfa469
Carmen Tawalika <tawalika@mundialis.de>
2019-03-05 17:04:07 +0100

    add auth and user in interface init for tests

--- src/openeo_grass_gis_driver/actinia_processing/actinia_interface.py
+++ src/openeo_grass_gis_driver/actinia_processing/actinia_interface.py
@@ -25,6 +25,8 @@ class ActiniaInterface(object):
         self.host = config.HOST
         self.port = config.PORT
         self.base_url = "%(host)s:%(port)s/latest" % {"host": self.host, "port": self.port}
+        self.auth = (config.USER, config.PASSWORD)
+        self.user = config.USER
 
     def set_auth(self, user: str, password: str):
         self.auth = (user, password)

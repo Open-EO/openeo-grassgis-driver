9ede717eb2e755b45352a44d411141d9bc92e3d7
huhabla <mo6i98ad>
2019-04-15 02:57:29 +0200

    Fixed recursive mode check

--- src/openeo_grass_gis_driver/actinia_processing/raster_exporter.py
+++ src/openeo_grass_gis_driver/actinia_processing/raster_exporter.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from random import randint
 import json
-from openeo_grass_gis_driver.actinia_processing.base import process_node_to_actinia_process_chain,\
-    PROCESS_DICT, PROCESS_DESCRIPTION_DICT, ProcessNode
+from openeo_grass_gis_driver.actinia_processing.base import process_node_to_actinia_process_chain, \
+    PROCESS_DICT, PROCESS_DESCRIPTION_DICT, Node, check_node_parents
 from openeo_grass_gis_driver.process_schemas import Parameter, ProcessDescription, ReturnValue
 from openeo_grass_gis_driver.actinia_processing.actinia_interface import ActiniaInterface
 
@@ -81,16 +81,15 @@ def create_process_chain_entry(input_name):
     return pc
 
 
-def get_process_list(node: ProcessNode):
+def get_process_list(node: Node):
     """Analyse the process description and return the Actinia process chain and the name of the processing result layer
     which is a single raster layer
 
-    :param args: The process description
+    :param node: The process node
     :return: (output_names, actinia_process_list)
     """
 
-    # Get the input description and the process chain to attach this process
-    input_names, process_list = process_node_to_actinia_process_chain(node)
+    input_names, process_list = check_node_parents(node=node)
     output_names = []
 
     # Pipe the inputs to the outputs

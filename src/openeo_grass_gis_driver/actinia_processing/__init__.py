62fc85bd19f3fef94546aaf3e277c30feff465f2
huhabla <mo6i98ad>
2019-04-28 23:35:53 +0200

    Added bbox from raster and rgb export

--- src/openeo_grass_gis_driver/actinia_processing/__init__.py
+++ src/openeo_grass_gis_driver/actinia_processing/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Import the actinia_processing to fill the process.PROCESS_DICT with actinia_processing
 from . import filter_bbox_process
+from . import bbox_from_raster_process
 from . import filter_daterange_process
 from . import get_data_process
 from . import ndvi_process
@@ -8,5 +9,6 @@ from . import map_algebra_process
 from . import reduce_time_process
 # from . import udf_reduce_time
 from . import raster_exporter
+from . import rgb_raster_exporter
 from . import zonal_statistics
 from . import temporal_algebra_process

3a688703d17ff758cf65f3ce89da846f60ff9933
huhabla <mo6i98ad>
2019-04-28 14:47:48 +0200

    Fixed process preview tests

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -4,7 +4,7 @@ import time
 import pprint
 from flask import json
 from openeo_grass_gis_driver.test_base import TestBase
-from openeo_grass_gis_driver.utils.process_graph_examples_v03 import *
+from openeo_grass_gis_driver.utils.process_graph_examples_v04 import *
 
 
 __license__ = "Apache License, Version 2.0"
@@ -24,8 +24,7 @@ class PreviewTestCase(TestBase):
     def test_graph_filter_bbox_nc_job_ephemeral(self):
         """Test the filter box process
         """
-        PREVIEW_TEMPLATE["process_graph"] = FILTER_BOX
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        response = self.app.post('/preview', data=json.dumps(FILTER_BBOX), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
@@ -36,10 +35,9 @@ class PreviewTestCase(TestBase):
         """Test the filter box process that produces an error because of wrong raster name
         """
 
-        fbox = FILTER_BOX
-        fbox["process_graph"]["imagery"]["data_id"] = "nc_spm_08.PERMANENT.raster.elevation_nonon"
-        PREVIEW_TEMPLATE["process_graph"] = fbox
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        fbox = FILTER_BBOX
+        fbox["process_graph"]["get_data_1"]["arguments"]["data"] = "nc_spm_08.PERMANENT.raster.elevation_nonon"
+        response = self.app.post('/preview', data=json.dumps(fbox), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
@@ -49,8 +47,7 @@ class PreviewTestCase(TestBase):
     def test_graph_get_data_1(self):
         """Test the get data process to get raster data
         """
-        PREVIEW_TEMPLATE["process_graph"] = GET_DATA_1
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        response = self.app.post('/preview', data=json.dumps(GET_DATA_1), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
@@ -60,8 +57,7 @@ class PreviewTestCase(TestBase):
     def test_graph_get_data_3(self):
         """Test the get data process to get strds data
         """
-        PREVIEW_TEMPLATE["process_graph"] = GET_DATA_3
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        response = self.app.post('/preview', data=json.dumps(GET_DATA_3), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
@@ -71,8 +67,7 @@ class PreviewTestCase(TestBase):
     def test_graph_daterange(self):
         """Run the daterange process
         """
-        PREVIEW_TEMPLATE["process_graph"] = DATERANGE
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        response = self.app.post('/preview', data=json.dumps(DATERANGE), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
@@ -82,19 +77,17 @@ class PreviewTestCase(TestBase):
     def test_graph_raster_export(self):
         """Run the raster export process
         """
-        PREVIEW_TEMPLATE["process_graph"] = RASTER_EXPORT
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        response = self.app.post('/preview', data=json.dumps(RASTER_EXPORT), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)
 
         self.assertEqual(200, response.status_code)
 
-    def test_graph_raster_zonal_statistics_single(self):
+    def test_graph_raster_zonal_statistics(self):
         """Run the zonal statistics process
         """
-        PREVIEW_TEMPLATE["process_graph"] = ZONAL_STATISTICS_SINGLE
-        response = self.app.post('/preview', data=json.dumps(PREVIEW_TEMPLATE), content_type="application/json", headers=self.auth)
+        response = self.app.post('/preview', data=json.dumps(ZONAL_STATISTICS), content_type="application/json", headers=self.auth)
 
         data = json.loads(response.data.decode())
         pprint.pprint(data)

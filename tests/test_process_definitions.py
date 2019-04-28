a8eedd1cd71fcc2dd5c33089296abbea36b0dac4
huhabla <mo6i98ad>
2019-04-28 13:29:36 +0200

    Fixed process definition tests

--- tests/test_process_definitions.py
+++ tests/test_process_definitions.py
@@ -3,8 +3,8 @@ import unittest
 from pprint import pprint
 from openeo_grass_gis_driver.actinia_processing import config
 from openeo_grass_gis_driver.test_base import TestBase
-from openeo_grass_gis_driver.actinia_processing.base import process_node_to_actinia_process_chain
-from openeo_grass_gis_driver.utils.process_graph_examples_v03 import *
+from openeo_grass_gis_driver.actinia_processing.base import process_node_to_actinia_process_chain, Node, Graph
+from openeo_grass_gis_driver.utils.process_graph_examples_v04 import *
 
 __license__ = "Apache License, Version 2.0"
 __author__ = "Sören Gebbert"
@@ -17,145 +17,114 @@ class ProcessDefinitionTestCase(TestBase):
 
     def test_get_data_1(self):
 
-        output_names, pc = process_node_to_actinia_process_chain(graph=GET_DATA_1)
-        pprint(output_names)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 1)
+        g = Graph(GET_DATA_1)
+        
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 1)
 
     def test_get_data_2(self):
 
-        output_names, pc = process_node_to_actinia_process_chain(graph=GET_DATA_2)
-        pprint(output_names)
-        pprint(pc)
+        g = Graph(GET_DATA_2)
+        p = 0
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 1)
+            p += 1
 
-        self.assertEqual(len(pc), 2)
+        self.assertEqual(p, 2)
 
     def test_get_data_3(self):
 
-        output_names, pc = process_node_to_actinia_process_chain(graph=GET_DATA_3)
-        pprint(output_names)
-        pprint(pc)
+        g = Graph(GET_DATA_3)
 
-        self.assertEqual(len(pc), 1)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertTrue(pc[0]["module"] == "t.info")
+            self.assertEqual(len(pc), 1)
 
     def test_filter_bbox(self):
 
-        output_names, pc = process_node_to_actinia_process_chain(graph=FILTER_BOX)
-        pprint(output_names)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 2)
-        self.assertTrue(pc[1]["module"] == "g.region")
+        g = Graph(FILTER_BBOX)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 2)
+            self.assertTrue(pc[0]["module"] == "r.info")
+            self.assertTrue(pc[1]["module"] == "g.region")
 
     def test_daterange(self):
 
-        output_names, pc = process_node_to_actinia_process_chain(graph=DATERANGE)
-        pprint(output_names)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 2)
-
-        self.assertTrue(pc[1]["module"] == "t.rast.extract")
+        g = Graph(DATERANGE)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 2)
+            self.assertTrue(pc[0]["module"] == "t.info")
+            self.assertTrue(pc[1]["module"] == "t.rast.extract")
 
     def test_reduce_time_min(self):
 
-        name, pc = process_node_to_actinia_process_chain(graph=REDUCE_TIME_MIN)
-        pprint(name)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 2)
+        g = Graph(REDUCE_TIME_MIN)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 2)
+            self.assertTrue(pc[0]["module"] == "t.info")
+            self.assertTrue(pc[1]["module"] == "t.rast.series")
 
     def test_ndvi_1(self):
 
-        names, pc = process_node_to_actinia_process_chain(graph=NDVI_1)
-        pprint(names)
-        pprint(pc)
 
-        self.assertEqual(names[0], "lsat5_red_NDVI")
-        self.assertEqual(len(pc), 4)
-
-    def test_ndvi_2(self):
-
-        names, pc = process_node_to_actinia_process_chain(graph=NDVI_2)
-        pprint(names)
-        pprint(pc)
-
-        self.assertEqual(names[0], "S2A_B04_NDVI")
-        self.assertEqual(len(pc), 4)
-
-    def test_ndvi_3(self):
-
-        names, pc = process_node_to_actinia_process_chain(graph=NDVI_3)
-        pprint(names)
-        pprint(pc)
-
-        self.assertEqual(names[0], "lsat5_red_NDVI2")
-        self.assertEqual(len(pc), 4)
-
-    def test_ndvi_4(self):
-
-        names, pc = process_node_to_actinia_process_chain(graph=NDVI_4)
-        pprint(names)
-        pprint(pc)
-
-        self.assertEqual(names[0], "S2A_B04_NDVI2")
-        self.assertEqual(len(pc), 4)
+        g = Graph(NDVI_STRDS)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 4)
+            self.assertTrue(pc[0]["module"] == "t.info")
+            self.assertTrue(pc[1]["module"] == "t.info")
+            self.assertTrue(pc[2]["module"] == "t.rast.mapcalc")
+            self.assertTrue(pc[3]["module"] == "t.rast.colors")
+            self.assertTrue("lsat5_red_NDVI" in output_names)
 
     def test_raster_export(self):
 
-        names, pc = process_node_to_actinia_process_chain(graph=RASTER_EXPORT)
-        pprint(names)
-        pprint(pc)
-
-        self.assertEqual(names[0], "nc_spm_08.PERMANENT.raster.elevation")
-        self.assertEqual(names[1], "nc_spm_08.PERMANENT.raster.slope")
-        self.assertEqual(len(pc), 4)
+        g = Graph(RASTER_EXPORT)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertTrue("nc_spm_08.PERMANENT.raster.elevation" in output_names)
+            self.assertEqual(len(pc), 2)
 
     def test_zonal_statistics(self):
 
-        names, pc = process_node_to_actinia_process_chain(graph=ZONAL_STATISTICS)
-        pprint(names)
-        pprint(pc)
-
-        self.assertEqual(names[1], "latlong_wgs84.asia_gdd_2017.strds.gdd")
-        self.assertEqual(names[0], "latlong_wgs84.modis_ndvi_global.strds.ndvi_16_5600m")
-        self.assertEqual(len(pc), 16)
-
-    def test_ndvi_error(self):
-
-        try:
-            names, pc = process_node_to_actinia_process_chain(graph=NDVI_ERROR)
-            pprint(names)
+        g = Graph(ZONAL_STATISTICS)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
             pprint(pc)
-            self.assertTrue(False)
-        except:
-            pass
+            self.assertTrue("latlong_wgs84.modis_ndvi_global.strds.ndvi_16_5600m" in output_names)
+            self.assertEqual(len(pc), 8)
 
     def test_openeo_usecase_1(self):
 
-        name, pc = process_node_to_actinia_process_chain(graph=OPENEO_USECASE_1)
-        pprint(name)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 9)
-
-    def test_openeo_usecase_1a(self):
-
-        name, pc = process_node_to_actinia_process_chain(graph=OPENEO_USECASE_1A)
-        pprint(name)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 8)
-
-    def otest_openeo_usecase_2(self):
-        # Disabled since UDF is not supported
-
-        name, pc = process_node_to_actinia_process_chain(graph=OPENEO_USECASE_2)
-        pprint(name)
-        pprint(pc)
-
-        self.assertEqual(len(pc), 6)
+        g = Graph(USE_CASE_1)
+        for node in g.root_nodes:
+            output_names, pc = process_node_to_actinia_process_chain(node=node)
+            pprint(output_names)
+            pprint(pc)
+            self.assertEqual(len(pc), 8)
 
 
 if __name__ == "__main__":

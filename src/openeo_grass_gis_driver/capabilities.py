b5487fe702fdc57d6ae71333fb37118b5fcffec6
Soeren Gebbert <soerengebbert@gmail.com>
2019-09-01 23:40:18 +0200

    Modified version of the supported api

--- src/openeo_grass_gis_driver/capabilities.py
+++ src/openeo_grass_gis_driver/capabilities.py
@@ -9,7 +9,7 @@ __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
 CAPABILITIES = {
-    "api_version": "0.4.0",
+    "api_version": "0.4.2",
     "backend_version": "0.1.0",
     "title": "GRASS GIS Driver",
     "description": "GRASS GIS Driver",

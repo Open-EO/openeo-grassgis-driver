6c24f4dfaa6bcae43ea54d6da36cfe14e71944ad
Andreas Schmitz <schmitz@terrestris.de>
2019-09-02 10:51:31 +0200

    Fix usage of the x-forwarded-* headers

--- src/openeo_grass_gis_driver/app.py
+++ src/openeo_grass_gis_driver/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 __author__ = "Sören Gebbert"
 __copyright__ = "Copyright 2018, Sören Gebbert, mundialis"
@@ -9,5 +10,6 @@ __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
 flask_app = Flask(__name__)
+flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app, x_for=1, x_host=1, x_proto=1)
 CORS(flask_app)
 flask_api = Api(flask_app)

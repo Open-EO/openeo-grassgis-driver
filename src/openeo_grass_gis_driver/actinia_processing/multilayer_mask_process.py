cb9eaddbf2201eccc1f1fbce5d0eb8f2f986cbe9
Soeren Gebbert <soerengebbert@gmail.com>
2019-09-03 01:11:42 +0200

    Fixed process example

--- src/openeo_grass_gis_driver/actinia_processing/multilayer_mask_process.py
+++ src/openeo_grass_gis_driver/actinia_processing/multilayer_mask_process.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from random import randint
 import json
+
+from openeo_grass_gis_driver.models.process_graph_schemas import ProcessGraph, ProcessGraphNode
+
 from openeo_grass_gis_driver.actinia_processing.base import Node, check_node_parents
 from openeo_grass_gis_driver.actinia_processing.base import PROCESS_DICT, PROCESS_DESCRIPTION_DICT
-from openeo_grass_gis_driver.models.process_schemas import Parameter, ProcessDescription, ReturnValue
+from openeo_grass_gis_driver.models.process_schemas import Parameter, ProcessDescription, ReturnValue, ProcessExample
 from openeo_grass_gis_driver.actinia_processing.actinia_interface import ActiniaInterface
 
 from flask import make_response, jsonify, request
@@ -26,16 +29,12 @@ def create_process_description():
     rv = ReturnValue(description="Multilayer mask as EO data.",
                      schema={"type": "object", "format": "eodata"})
 
-    simple_example = {
-        "multilayer_mask_1": {
-            "process_id": PROCESS_NAME,
-            "arguments": {
-                "data": {"from_node": "get_strds_data"},
-            }
-        }
-    }
-
-    examples = dict(simple_example=simple_example)
+    # Example
+    arguments = {"data": {"from_node": "get_strds_data"}}
+    node = ProcessGraphNode(process_id=PROCESS_NAME, arguments=arguments)
+    graph = ProcessGraph(title="title", description="description", process_graph={"multilayer_mask_1": node})
+    examples = [ProcessExample(title="Simple example", description="Simple example",
+                               process_graph=graph, arguments=arguments)]
 
     pd = ProcessDescription(id=PROCESS_NAME,
                             description="Creates a mask using several bands of an EO dataset. "
@@ -63,38 +62,38 @@ def create_process_chain_entry(input_time_series, output_name):
     """
     input_name = ActiniaInterface.layer_def_to_grass_map_name(input_time_series)
     output_name_tmp = output_name + '_tmp'
-    
+
     # get number of maps in input_time_series
     iface = ActiniaInterface()
     status_code, layer_data = iface.layer_info(layer_name=input_time_series)
     if status_code != 200:
         return make_response(jsonify({"description": "An internal error occurred "
-                                                         "while catching GRASS GIS layer information "
-                                                         "for layer <%s>!\n Error: %s"
-                                                         "" % (input_time_series, str(layer_data))}, 400))
+                                                     "while catching GRASS GIS layer information "
+                                                     "for layer <%s>!\n Error: %s"
+                                                     "" % (input_time_series, str(layer_data))}, 400))
     nmaps = layer_data['number_of_maps']
 
     rn = randint(0, 1000000)
 
     pc = [
-         {"id": "t_rast_series_%i" % rn,
-          "module": "t.rast.series",
-          "inputs": [{"param": "input", "value": input_name},
-                     {"param": "method", "value": "count"},
-                     {"param": "output", "value": output_name_tmp}],
-          "flags": "t"},
+        {"id": "t_rast_series_%i" % rn,
+         "module": "t.rast.series",
+         "inputs": [{"param": "input", "value": input_name},
+                    {"param": "method", "value": "count"},
+                    {"param": "output", "value": output_name_tmp}],
+         "flags": "t"},
 
-         {"id": "r_mapcalc_%i" % rn,
-          "module": "r.mapcalc",
-          "inputs": [{"param": "expression",
-                     "value": "%(result)s = int(if(%(raw)s < %(nmaps)s, 1, 0))" % 
-                                            {"result": output_name,
-                                             "raw": output_name_tmp,
-                                             "nmaps": str(nmaps)}}
+        {"id": "r_mapcalc_%i" % rn,
+         "module": "r.mapcalc",
+         "inputs": [{"param": "expression",
+                     "value": "%(result)s = int(if(%(raw)s < %(nmaps)s, 1, 0))" %
+                              {"result": output_name,
+                               "raw": output_name_tmp,
+                               "nmaps": str(nmaps)}}
                     ],
          }]
     # g.remove raster name=output_name_tmp -f ?
-         
+
     return pc
 
 

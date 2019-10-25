e86889b545dcf162ef6eb89a037ba2ef49e8a526
Soeren Gebbert <soerengebbert@gmail.com>
2019-10-25 11:36:09 +0200

    New data object to store grass layers in a more convenient way for input and output handling

--- src/openeo_grass_gis_driver/actinia_processing/apply_mask_process.py
+++ src/openeo_grass_gis_driver/actinia_processing/apply_mask_process.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from random import randint
 import json
-from openeo_grass_gis_driver.actinia_processing.base import Node, check_node_parents
+from openeo_grass_gis_driver.actinia_processing.base import Node, check_node_parents, DataObject, GrassDataType
 from openeo_grass_gis_driver.actinia_processing.base import PROCESS_DICT, PROCESS_DESCRIPTION_DICT
 from openeo_grass_gis_driver.models.process_graph_schemas import ProcessGraphNode, ProcessGraph
 from openeo_grass_gis_driver.models.process_schemas import Parameter, ProcessDescription, ReturnValue, ProcessExample
@@ -55,20 +55,18 @@ def create_process_description():
 PROCESS_DESCRIPTION_DICT[PROCESS_NAME] = create_process_description()
 
 
-def create_process_chain_entry(input_name, mask_name, output_name):
+def create_process_chain_entry(input: DataObject, mask: DataObject, output: DataObject):
     """Create a Actinia process description that uses r.mapcalc 
        to apply a mask.
        
        NOTE: according to multilayer_mask, 0 means not masked, 1 means masked
              this is the other way around compared to GRASS
 
-    :param input_name: The input map name
-    :param mask_name: The mask map name
-    :param output_name: The name of the output map
+    :param input: The input map name
+    :param mask: The mask map name
+    :param output: The name of the output map
     :return: A Actinia process chain description
     """
-    input_name = ActiniaInterface.layer_def_to_grass_map_name(input_name)
-    mask_name = ActiniaInterface.layer_def_to_grass_map_name(mask_name)
     
     rn = randint(0, 1000000)
 
@@ -76,9 +74,9 @@ def create_process_chain_entry(input_name, mask_name, output_name):
          "module": "r.mapcalc",
          "inputs": [{"param": "expression",
                      "value": "%(result)s = if(isnull(%(mask)s) || %(mask)s == 0, "
-                              "null(), %(raw)s)" % {"result": output_name,
-                                                    "raw": input_name,
-                                                    "mask": mask_name}}
+                              "null(), %(raw)s)" % {"result": output.grass_name(),
+                                                    "raw": input.grass_name(),
+                                                    "mask": mask.grass_name()}}
                    ]}
 
     return pc
@@ -90,38 +88,38 @@ def get_process_list(node: Node):
     which is a single raster layer
 
     :param node: The process node
-    :return: (output_names, actinia_process_list)
+    :return: (output_objects, actinia_process_list)
     """
 
-    input_names, process_list = check_node_parents(node=node)
-    output_names = []
+    input_objects, process_list = check_node_parents(node=node)
+    output_objects = []
 
     if "mask" not in node.arguments:
         raise Exception("Parameter mask is required.")
 
-    nmasks = len(node.get_parent_by_name("mask").output_names)
-    ninputs = len(node.get_parent_by_name("data").output_names)
+    nmasks = len(node.get_parent_by_name("mask").output_objects)
+    ninputs = len(node.get_parent_by_name("data").output_objects)
 
     if nmasks > 1 and nmasks != ninputs:
         raise Exception("Either a single mask or a separate mask for each layer is required.")
 
-    mask_name = None
+    mask_object = None
     if nmasks == 1:
-        mask_name = node.get_parent_by_name("mask").output_names[0]
+        mask_object = node.get_parent_by_name("mask").output_objects[0]
 
-    for i in range(len(node.get_parent_by_name("data").output_names)):
-        input_name = node.get_parent_by_name("data").output_names[i]
+    for i in range(len(node.get_parent_by_name("data").output_objects)):
+        input_object = node.get_parent_by_name("data").output_objects[i]
         if nmasks > 1:
-            mask_name = node.get_parent_by_name("mask").output_names[i]
-        location, mapset, datatype, layer_name = ActiniaInterface.layer_def_to_components(input_name)
-        output_name = "%s_%s" % (layer_name, PROCESS_NAME)
-        output_names.append(output_name)
-        node.add_output(output_name=output_name)
+            mask_object = node.get_parent_by_name("mask").output_objects[i]
 
-        pc = create_process_chain_entry(input_name, mask_name, output_name)
+        output_object = DataObject(name=f"{input_object.name}_{PROCESS_NAME}", datatype=GrassDataType.RASTER)
+        output_objects.append(output_object)
+        node.add_output(output_object=output_object)
+
+        pc = create_process_chain_entry(input_object, mask_object, output_object)
         process_list.append(pc)
 
-    return output_names, process_list
+    return output_objects, process_list
 
 
 PROCESS_DICT[PROCESS_NAME] = get_process_list

1d2d8a8762fd7d00934fbd7d557c9dacb3907e9a
Markus Metz <33666869+metzm@users.noreply.github.com>
2019-10-28 13:55:47 +0100

    Update reduce_time_process.py
    Output is raster, not strds

--- src/openeo_grass_gis_driver/actinia_processing/reduce_time_process.py
+++ src/openeo_grass_gis_driver/actinia_processing/reduce_time_process.py
@@ -95,7 +95,7 @@ def get_process_list(node: Node):
 
     for input_object in node.get_parent_by_name("data").output_objects:
 
-        output_object = DataObject(name=f"{input_object.name}_{PROCESS_NAME}", datatype=GrassDataType.STRDS)
+        output_object = DataObject(name=f"{input_object.name}_{PROCESS_NAME}", datatype=GrassDataType.RASTER)
         output_objects.append(output_object)
         node.add_output(output_object=output_object)
 

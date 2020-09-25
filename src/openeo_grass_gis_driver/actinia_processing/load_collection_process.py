7c1074a75a9ee0b9ed1ac315feadabfb5d229b33
Markus <neteler@gmail.com>
2020-09-25 15:50:26 +0200

    PEP8: fix E124: closing bracket does not match visual indentation

--- src/openeo_grass_gis_driver/actinia_processing/load_collection_process.py
+++ src/openeo_grass_gis_driver/actinia_processing/load_collection_process.py
@@ -188,7 +188,7 @@ def create_process_description():
                      "2018-01-01",
                      "2019-01-01"
                  ],
-        }
+                 }
     node = ProcessGraphNode(process_id=PROCESS_NAME, arguments=arguments)
     graph = ProcessGraph(title="title", description="description", process_graph={"load_strds_collection": node})
     examples = [ProcessExample(title="Simple example", description="Simple example",

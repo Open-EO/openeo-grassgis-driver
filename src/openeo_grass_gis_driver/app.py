c15a586f39af58b58568942c9336c4c448ca917a
Carmen <carmen.tawalika@gmail.com>
2020-11-06 13:54:14 +0100

    add explanation

--- src/openeo_grass_gis_driver/app.py
+++ src/openeo_grass_gis_driver/app.py
@@ -11,5 +11,7 @@ __email__ = "soerengebbert@googlemail.com"
 
 flask_app = Flask(__name__)
 flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app, x_for=1, x_host=1, x_proto=1)
+# as flaks CORS lead to unforeseen behaviour in certain cases, we configure
+# headers with nginx and outcomment here.
 # CORS(flask_app, supports_credentials=True)
 flask_api = Api(flask_app)

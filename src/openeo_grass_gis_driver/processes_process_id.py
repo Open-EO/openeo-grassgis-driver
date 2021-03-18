3c0133a5561078b9746c162db663339774f1e429
Markus Metz <markus.metz.giswork@gmail.com>
2021-03-18 22:07:31 +0100

    openeo GRASS GIS driver
     * fix test_async_persistent_processing()
     * add actinia processes to /processes/<process_id>

--- src/openeo_grass_gis_driver/processes_process_id.py
+++ src/openeo_grass_gis_driver/processes_process_id.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import make_response, jsonify
 from openeo_grass_gis_driver.actinia_processing.base import PROCESS_DESCRIPTION_DICT
+from openeo_grass_gis_driver.actinia_processing.base import \
+    ACTINIA_PROCESS_DESCRIPTION_DICT
 from openeo_grass_gis_driver.authentication import ResourceBase
 
 __license__ = "Apache License, Version 2.0"
@@ -17,7 +19,9 @@ class ProcessesProcessId(ResourceBase):
 
     def get(self, process_id):
 
-        if process_id not in PROCESS_DESCRIPTION_DICT:
-            return make_response(jsonify({"description": "This process does not exist!"}), 400)
+        if process_id in PROCESS_DESCRIPTION_DICT:
+            return make_response(jsonify(PROCESS_DESCRIPTION_DICT[process_id]), 200)
+        elif process_id in ACTINIA_PROCESS_DESCRIPTION_DICT:
+            return make_response(jsonify(ACTINIA_PROCESS_DESCRIPTION_DICT[process_id]), 200)
 
-        return make_response(jsonify(PROCESS_DESCRIPTION_DICT[process_id]), 200)
+        return make_response(jsonify({"description": "This process does not exist!"}), 400)

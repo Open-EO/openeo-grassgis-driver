5a05917bdab5f1a0a1d7ee7dce9e05c0b5fc3ee4
Carmen <carmen.tawalika@gmail.com>
2021-03-24 13:42:47 +0100

    E225

--- src/openeo_grass_gis_driver/actinia_processing/config.py
+++ src/openeo_grass_gis_driver/actinia_processing/config.py
@@ -10,18 +10,18 @@ __email__ = "soerengebbert@googlemail.com"
 
 class Config:
     # Settings for docker swarm image
-    HOST="https://actinia-dev.mundialis.de"
-    PORT=443
-    LOCATIONS=["nc_spm_08", "utm32n", "latlong_wgs84"]
-    #USER="demouser"
-    #PASSWORD="gu3st!pa55w0rd"
-    USER="openeo"
-    PASSWORD="EeMob0la"
+    HOST = "https://actinia-dev.mundialis.de"
+    PORT = 443
+    LOCATIONS = ["nc_spm_08", "utm32n", "latlong_wgs84"]
+    #USER = "demouser"
+    #PASSWORD = "gu3st!pa55w0rd"
+    USER = "openeo"
+    PASSWORD = "EeMob0la"
     # The database file that stores the graphs
-    GRAPH_DB="%s/.graph_db_file.sqlite" %os.environ["HOME"]
+    GRAPH_DB = "%s/.graph_db_file.sqlite" % os.environ["HOME"]
     # The database file that stores the jobs
-    JOB_DB="%s/.job_db_file.sqlite" %os.environ["HOME"]
-    TOKEN_DB="%s/.actinia_auth_tokens.sqlite" %os.environ["HOME"]
+    JOB_DB = "%s/.job_db_file.sqlite" % os.environ["HOME"]
+    TOKEN_DB = "%s/.actinia_auth_tokens.sqlite" % os.environ["HOME"]
     # The database file that stores the actinia jobs
-    ACTINIA_JOB_DB="%s/.actinia_job_db_file.sqlite" %os.environ["HOME"]
-    SECRET_KEY="jaNguzeef4seiv5shahchimoo8teiLah"
+    ACTINIA_JOB_DB = "%s/.actinia_job_db_file.sqlite" % os.environ["HOME"]
+    SECRET_KEY = "jaNguzeef4seiv5shahchimoo8teiLah"

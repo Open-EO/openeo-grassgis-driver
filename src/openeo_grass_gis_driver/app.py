f2f2f5bd7fb1a27b787d390c644bb8de6dc28ad7
Carmen <carmen.tawalika@gmail.com>
2021-04-29 16:59:10 +0200

    reactivate CORS

--- src/openeo_grass_gis_driver/app.py
+++ src/openeo_grass_gis_driver/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-# from flask_cors import CORS
+from flask_cors import CORS
 from flask_restful import Api
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -11,7 +11,6 @@ __email__ = "soerengebbert@googlemail.com"
 
 flask_app = Flask(__name__)
 flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app, x_for=1, x_host=1, x_proto=1)
-# as flask CORS lead to unforeseen behaviour in certain cases, we configure
-# headers with nginx and outcomment here.
 # CORS(flask_app, supports_credentials=True)
+CORS(flask_app)
 flask_api = Api(flask_app)

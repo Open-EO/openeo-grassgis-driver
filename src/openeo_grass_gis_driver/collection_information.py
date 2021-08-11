a8a07df9095985d8e2dc08fb870404fa7c39c07c
Markus Metz <markus.metz.giswork@gmail.com>
2021-08-11 19:51:29 +0200

    open-grass-driver
    * fix the fix for collection_information

--- src/openeo_grass_gis_driver/collection_information.py
+++ src/openeo_grass_gis_driver/collection_information.py
@@ -204,36 +204,45 @@ class CollectionInformationResource(Resource):
                         layer_data["north"])), temporal=(
                             start_time, end_time))
 
-            # TODO: band_names must be in layer_data
+            # TODO: band_names must be in layer_data coming from actinia
             if "number_of_bands" in layer_data:
                 if layer_data["number_of_bands"] == "13":
                     bands.append(EOBands(name="S2_1", common_name="coastal"))
                     bands.append(EOBands(name="S2_2", common_name="blue"))
                     bands.append(EOBands(name="S2_3", common_name="green"))
                     bands.append(EOBands(name="S2_4", common_name="red"))
-                    bands.append(EOBands(name="S2_5", common_name="rededge"))
-                    bands.append(EOBands(name="S2_6", common_name="rededge"))
-                    bands.append(EOBands(name="S2_7", common_name="rededge"))
+                    bands.append(EOBands(name="S2_5", common_name="rededge1"))
+                    bands.append(EOBands(name="S2_6", common_name="rededge2"))
+                    bands.append(EOBands(name="S2_7", common_name="rededge3"))
                     bands.append(EOBands(name="S2_8", common_name="nir"))
                     bands.append(EOBands(name="S2_8A", common_name="nir08"))
                     bands.append(EOBands(name="S2_9", common_name="nir09"))
                     bands.append(EOBands(name="S2_10", common_name="cirrus"))
                     bands.append(EOBands(name="S2_11", common_name="swir16"))
                     bands.append(EOBands(name="S2_12", common_name="swir22"))
+                    bands.append(EOBands(name="S2_AOT", common_name="aot"))
+                    bands.append(EOBands(name="S2_SCL", common_name="scl"))
+                    bands.append(EOBands(name="S2_TCI", common_name="tci"))
+                    bands.append(EOBands(name="S2_wvp", common_name="wvp"))
 
                     dimensions['bands'] = {"type": "bands",
                                            "values": ["coastal",
                                                       "blue",
                                                       "green",
-                                                      "rededge",
-                                                      "rededge",
-                                                      "rededge",
+                                                      "red",
+                                                      "rededge1",
+                                                      "rededge2",
+                                                      "rededge3",
                                                       "nir",
                                                       "nir08",
                                                       "nir09",
                                                       "cirrus",
                                                       "swir16",
-                                                      "swir22"
+                                                      "swir22",
+                                                      "aot",
+                                                      "scl",
+                                                      "tci",
+                                                      "wvp"
                                                       ]
                                            }
 

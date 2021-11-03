57b309819fdc456fba02cd1ab8fe6731ddfbb66a
Markus Metz <33666869+metzm@users.noreply.github.com>
2021-11-03 10:08:00 +0100

    openeo GRASS driver: improve GRASS module wrapping (#123)
    Hide the GRASS output option `basename` from openeo users and set it automatically in order to conform to openeo process syntax.
    
    Co-authored-by: Marc Jansen <jansen@terrestris.de>

--- src/openeo_grass_gis_driver/register_actinia_processes.py
+++ src/openeo_grass_gis_driver/register_actinia_processes.py
@@ -3,7 +3,8 @@ from openeo_grass_gis_driver.actinia_processing.config import Config \
     as ActiniaConfig
 from openeo_grass_gis_driver.actinia_processing.base import \
     ACTINIA_OPENEO_PROCESS_DESCRIPTION_DICT, \
-    OPENEO_ACTINIA_ID_DICT
+    OPENEO_ACTINIA_ID_DICT, \
+    T_BASENAME_MODULES_LIST
 from openeo_grass_gis_driver.actinia_processing.actinia_interface import \
     ActiniaInterface
 
@@ -27,6 +28,17 @@ def register_processes():
         print("Registering modules...")
         for module in modules:
             # convert grass module names to openeo process names
+            # special treatment for GRASS modules in
+            # T_BASENAME_MODULES_LIST
+            if module["id"] in T_BASENAME_MODULES_LIST:
+                if "parameters" in module:
+                    module["parameters"] = [
+                        i for i in module["parameters"] if i["name"] != "basename"
+                    ]
+                if "returns" in module:
+                    module["returns"] = [
+                        i for i in module["returns"] if i["name"] != "basename"
+                    ]
             process = module["id"].replace('.', '_')
             actiniaid = module["id"]
             if "parameters" in module:
@@ -49,6 +61,10 @@ def register_processes():
                 # create "pseudo" module for every output:
                 for returns in module['returns']:
                     pm = dict(module)
+                    pm["links"] = dict()
+                    pm["links"]["about"] = (
+                        "https://grass.osgeo.org/grass80/manuals/%s.html" % pm["id"]
+                    )
                     pm['returns'] = returns
                     process = "%s_%s" % (
                         pm['id'].replace('.', '_'), returns['name'])
@@ -60,6 +76,10 @@ def register_processes():
                     }
             else:
                 # if no output, assign empty object
+                module["links"] = dict()
+                module["links"]["about"] = (
+                    "https://grass.osgeo.org/grass80/manuals/%s.html" % module["id"]
+                )
                 module['returns'] = {}
                 module["id"] = process
                 OPENEO_ACTINIA_ID_DICT[process] = {"id": actiniaid}
